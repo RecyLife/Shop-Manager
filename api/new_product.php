75be134b81210901033f7bd02fcec94a0ba3a99d
JdM <julien@rmbi.ch>
2024-10-12 15:46:55 +0200

    Refactor new_product.php and index.html to enforce admin password validation, use prepared statements for inserting specifications, and allow uploading multiple images

--- api/new_product.php
+++ api/new_product.php
@@ -4,6 +4,9 @@ include_once(dirname(__FILE__) . "/utils/database.php");
 include_once(dirname(__FILE__). "/secrets/database.php");
 
 $db = new Database;
+const ALLOWED_IMAGES_EXT = ["png", "jpg", "jpeg"];
+const ALLOWED_IMAGES_MIMES = array("image/jpeg", "image/png");
+const MAX_IMAGES_SIZE = 2 * 1000 * 1000; // 2 MB
 
 $password = $_POST["password"];
 if($password != ADMIN_PASSWORD) {
@@ -30,14 +33,46 @@ $specifications =  json_decode($_POST["specifications"], true);
 $db->query("INSERT INTO recytech_products (title, price, category_ID) VALUES (?, ?, ?)", [$title, $price, $category]);
 $productID = $db -> getLastInsertedID();
 
-
 for($i = 0; $i < count($specifications); $i++) {
-    if($specTitle!= "" & $specValue != "") {
-        $specTitle = $db -> escapeStrings(htmlspecialchars($specifications[$i]["title"]));
-        $specValue = $db -> escapeStrings(htmlspecialchars($specifications[$i]["value"]));
+    $specTitle = $db -> escapeStrings(htmlspecialchars($specifications[$i]["title"]));
+    $specValue = $db -> escapeStrings(htmlspecialchars($specifications[$i]["value"]));
+    
+    if($specTitle != "" && $specValue != "") {
+        $db -> query("INSERT into recytech_specifications (title, value_, product_ID) VALUES (?, ?, ?)", [$specTitle, $specValue, $productID]);
     }
-
-    $db -> query("INSERT into recytech_specifications (title, value_, product_ID) VALUES (?, ?, ?)", [$specTitle, $specValue, $productID]);
 }
 
-header("location: ../admin");
+foreach ($_FILES["images"]["tmp_name"] as $key => $tmp_name) {
+    $temp = $_FILES["images"]["tmp_name"][$key];
+    $name = $_FILES["images"]["name"][$key];
+    $size = $_FILES["images"]["size"][$key];
+
+    if (empty($temp)) {
+        break;
+    }
+
+    $ext = pathinfo($name, PATHINFO_EXTENSION);
+    if (!in_array($ext, ALLOWED_IMAGES_EXT)) {
+        echo json_encode(array("error"=> "invalid image format (0)"));
+        exit();
+    }
+
+    $mime = mime_content_type($temp);
+    if(!in_array($mime, ALLOWED_IMAGES_MIMES)) {
+        echo "invalid image format (1)";
+        echo $mime;
+        exit();
+    }
+
+    if ($size > MAX_IMAGES_SIZE) {
+        echo json_encode(array("error"=> "image size exceeds the maximum limit of 5 MB"));
+        exit();
+    }
+
+    $imageContent = file_get_contents($temp);
+
+    $db -> query("INSERT into recytech_images (image_, product_ID) VALUES (?, ?)", [$imageContent, $productID]);
+
+}
+
+// header("location: ../admin");
