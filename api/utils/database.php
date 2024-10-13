2ccf957ca652c6f7df663286e595c59ab4c416c5
JdM <julien@rmbi.ch>
2024-10-13 17:47:29 +0200

    Fix error reporting and include admin password check in new_product.php

--- api/utils/database.php
+++ api/utils/database.php
@@ -7,7 +7,7 @@ class Database
     public function __construct()
     {
         include_once(dirname(__FILE__) . "/secrets.php");
-        $this->conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+        $this->conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST_PORT);
 
         if ($this->conn->connect_error) {
             die("Connection failed: " . $this->conn->connect_error);
--- new_product/new_product.js
+++ new_product/new_product.js
@@ -38,7 +38,7 @@ function onFormSubmit() {
     const specificationsValuesInputs = document.getElementsByClassName("specificationValueInput")
     const specificationsTitlesInputs = document.getElementsByClassName("specificationTitleInput")
 
-    if(specificationsTitlesInputs[i].value != "" && specificationsValuesInputs.value != ""){
+    if(specificationsTitlesInputs.value != "" && specificationsValuesInputs.value != ""){
         let result = [];
         
         for (let i = 0; i < specificationsTitlesInputs.length; i++) {
