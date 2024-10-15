2a6e2e3befbb32a7554013411f99a3fcece5a154
judemont <julien@rmbi.ch>
2024-10-15 13:23:55 +0200

    refactor: Update API endpoint for retrieving products and include category information

---
+++ api/product.php
@@ -0,0 +1,29 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header('Content-Type: application/json');
+
+include_once(dirname(__FILE__) . "/utils/database.php");
+
+$db = new Database;
+
+if(!isset(($_GET["id"]))) {
+    exit();
+}
+
+$id = $db -> escapeStrings($_GET["id"]);
+
+$products = $db -> select("
+SELECT 
+    recytech_products.ID as ID, 
+    recytech_products.title as title, 
+    recytech_products.price as price,
+    recytech_categories.title as category
+from recytech_products
+INNER JOIN recytech_categories 
+    ON recytech_products.category_ID = recytech_categories.ID
+WHERE recytech_products.ID = $id");
+
+if(count($products) > 0) {
+    echo json_encode($products[0]);
+} 
