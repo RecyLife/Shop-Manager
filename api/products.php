807979b1cb5042cb1b94df49f7dfa6177c0611a9
judemont <julien@rmbi.ch>
2024-10-15 12:11:09 +0200

    chore: Update API endpoint for retrieving products and include category information

--- api/products.php
+++ api/products.php
@@ -7,6 +7,14 @@ include_once(dirname(__FILE__) . "/utils/database.php");
 
 $db = new Database;
 
-$products = $db -> select("SELECT * from recytech_products");
+$products = $db -> select("
+SELECT 
+    recytech_products.ID as ID, 
+    recytech_products.title as title, 
+    recytech_products.price as price,
+    recytech_categories.title as category
+from recytech_products
+INNER JOIN recytech_categories ON 
+recytech_products.category_ID=recytech_categories.ID");
 
 echo json_encode(array_values($products));
