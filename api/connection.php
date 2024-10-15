1b77d73c14d6674ef1ee755f216cce75257755ee
judemont <julien@rmbi.ch>
2024-10-15 14:09:14 +0200

    refactor: Add admin connection page and improve error handling in database.php

---
+++ api/connection.php
@@ -0,0 +1,15 @@
+<?php
+include_once(dirname(__FILE__) . "/utils/database.php");
+include_once(dirname(__FILE__). "/utils/secrets.php");
+
+$db = new Database;
+
+$password = $_POST["password"];
+
+if($password != ADMIN_PASSWORD) {
+    echo json_encode(array("error"=> "invalid admin password"));
+    exit();
+} else {
+    session_start();
+    $_SESSION["admin"] = "true";
+}
