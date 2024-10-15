b357669171bc67d8297de4ef28e57f019dfd547e
judemont <julien@rmbi.ch>
2024-10-15 13:17:29 +0200

    chore: Add product page and edit page links to products table in manage/index.html

--- manage/script.js
+++ manage/script.js
@@ -10,13 +10,21 @@ async function getProducts() {
 
     console.log(productsTitles)
 
-    const tr = document.createElement("tr");
+    tr = document.createElement("tr");
     let th;
     for (let i = 0; i < productsTitles.length; i++) {
         th = document.createElement("th");
         th.innerText = productsTitles[i];
         tr.appendChild(th);
     }
+    th = document.createElement("th");
+    th.innerText = "product page";
+    tr.appendChild(th);
+
+    th = document.createElement("th");
+    th.innerText = "edit page";
+    tr.appendChild(th);
+
     productsTable.appendChild(tr);
 
     for (let i = 0; i < products.length; i++) {
@@ -27,6 +35,15 @@ async function getProducts() {
             th.innerText = products[i][productsTitles[ii]]
             tr.appendChild(th)
         }
+        let th = document.createElement("th");
+        th.innerHTML = `<a href="https://shop.recytech.me/detail?id=${products[i].ID}">product page</a>`
+        tr.appendChild(th)
+
+        th = document.createElement("th");
+        console.log(products[i].ID)
+        th.innerHTML = `<a href='../edit?id=${products[i].ID}'>edit</a>`
+        tr.appendChild(th)
+
         productsTable.appendChild(tr);
     }
 }
